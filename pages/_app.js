245afdfabc4ed89d750cec472411abf08e443c2f
Aryane Nicéia <aryniceia@gmail.com>
2021-07-12 22:18:08 -0300

    Início do alurakut

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,34 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { AlurakutStyles } from '../src/lib/AlurakutCommons';
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  /* Reset CSS (Necolas Reset CSS) */
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
+
+  body {
+    font-family: sans-serif;
+    background: #D9E6F6;
+  }
+
+  #_next {
+    display: flex;
+    min-height: 100hv;
+    flex-direction: column;
+  }
+
+  /*Imagem responsiva por padrão*/
+    img {
+      max-width: 100%;
+      height: auto;
+      display: block;
+    }
+
+  ${AlurakutStyles}
+
 `
 
 const theme = {
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,66 @@
-import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid/index';
+import Box from '../src/components/Box/index';
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations/index'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+
+function ProfileSidebar (propriedades) {
+  console.log(propriedades)
+  return (
+    <Box >
+      <img src={`https://github.com/${propriedades.githubUser}.png`} style={{borderRadius: '8px'}} /> {/* Essa forma de passar var não é exclusivo de react mas sim de JS. O que é exclusividade react é a {} externa que dá pra usar JS dentro do html */}
+    </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const usuarioAleatorio = 'aryniceia';
+  const pessoasFavoritas = [
+    'HaluDeCassia', 
+    'aryniceia'
+  ]
+
+  return (
+    <>
+      <AlurakutMenu/>
+      <MainGrid>
+        {/* <Box style="grid-area: profileArea;"> */}
+        <div className="profileArea" style={{gridArea: 'profileArea'}}>
+          <ProfileSidebar githubUser={usuarioAleatorio}/> 
+        </div>
+
+        <div className="welcomeArea" style={{gridArea: 'welcomeArea'}}>
+          <Box >
+            <h1 className="Title">
+              Bem vinde
+            </h1>
+
+
+            <OrkutNostalgicIconSet/>
+          </Box>
+        </div>
+
+        <div className="profileRelationsArea" style={{gridArea: 'profileRelationsArea'}}>
+          <ProfileRelationsBoxWrapper >
+            <h2 className="smallTitle">
+              Pessoas da Comunidade ({pessoasFavoritas.length}) {/*Conta o número de amigos*/}
+            </h2>
+            
+            <ul>
+              {pessoasFavoritas.map((itemAtual) => {
+                return (
+                  <li>
+                    <a href={`/users/${itemAtual}`} key={itemAtual}>
+                      <img src={`https://github.com/${itemAtual}.png`} />
+                      <span>{itemAtual}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </>
+  )
 }
